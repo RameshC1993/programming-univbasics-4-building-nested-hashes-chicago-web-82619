f3a7ccb7f291ad3936753364e89de44815485638
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-23 18:47:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,9 @@ def base_hash
   {
     :railroad => {
       :pieces => 4,
-      :rent_in_dollers => 
+      :rent_in_dollers => {
+        
+      }
     }
   }# Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
