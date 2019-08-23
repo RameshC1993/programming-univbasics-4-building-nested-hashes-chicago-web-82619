03b7d0f8a451c57e3712d52a7aa2ba6d26033ce0
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-23 19:10:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        :one_pice_owned => 1,
+        :one_piece_owned => 1,
         :two => 2,
         :three => 3,
         :four => 4
